82a9d1e719735dda2ae6cc12812e0e533ece7e0f
Sergei Golubtsov <golubtsovsergei581study@gmail.com>
2020-07-21 14:46:32 +0300

    start

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const number = 33;
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p style={{
+          color: 'blue'
+        }}>Hello!</p>
+        <p>
+          {number}
+        </p>
+        <p>
+          {54}
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
