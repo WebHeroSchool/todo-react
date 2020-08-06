c8975366c7f1316936b280f0d317811c045c36e6
Sergei Golubtsov <golubtsovsergei581study@gmail.com>
2020-08-06 16:30:27 +0300

    refArrow

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -22,16 +22,16 @@ class App extends React.Component {
     ]
   };
 
-  onClickDone = isDone => console.log(isDone);
+  onClickDone = (isDone) => console.log(isDone);
 
   render () {
-    let unCompletedTasks = this.state.tasks.filter(task => task.isDone === false);
+    let uncompletedTasks = this.state.tasks.filter((task) => task.isDone === false);
     
       return (<div className={styles.wrap}>
         <h1 className={styles.title}>Важные дела:</h1>
         <InputItem />
         <ItemList tasks={this.state.tasks} onClickDone={this.onClickDone} />
-        <Footer count={unCompletedTasks.length} />
+        <Footer count={uncompletedTasks.length} />
       </div>)
   }
 };
