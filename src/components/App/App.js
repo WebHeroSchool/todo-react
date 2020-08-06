47700ff2ba74d9a9fea4254fc2b1c4accfe8c98a
Sergei Golubtsov <golubtsovsergei581study@gmail.com>
2020-08-06 12:48:16 +0300

    event-status

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -22,13 +22,15 @@ class App extends React.Component {
     ]
   }
 
+  onClickDone = isDone => console.log(isDone);
+
   render () {
     let unCompletedTasks = this.state.tasks.filter(task => task.isDone === false);
     
       return (<div className={styles.wrap}>
         <h1 className={styles.title}>Важные дела:</h1>
         <InputItem />
-        <ItemList tasks={this.state.tasks} />
+        <ItemList tasks={this.state.tasks} onClickDone={this.onClickDone} />
         <Footer count={unCompletedTasks.length} />
       </div>)
   }
