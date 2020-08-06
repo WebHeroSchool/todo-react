50a7415cc6ecd968f7644ddbad8b05ab1e8ead92
Sergei Golubtsov <golubtsovsergei581study@gmail.com>
2020-08-06 13:25:42 +0300

    refEvent

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -20,7 +20,9 @@ class App extends React.Component {
         isDone: true
       }
     ]
-  }
+  };
+
+  onClickDone = isDone => console.log(isDone);
 
   render () {
     let unCompletedTasks = this.state.tasks.filter(task => task.isDone === false);
@@ -28,7 +30,7 @@ class App extends React.Component {
       return (<div className={styles.wrap}>
         <h1 className={styles.title}>Важные дела:</h1>
         <InputItem />
-        <ItemList tasks={this.state.tasks} />
+        <ItemList tasks={this.state.tasks} onClickDone={this.onClickDone} />
         <Footer count={unCompletedTasks.length} />
       </div>)
   }
--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -3,16 +3,13 @@ import Item from '../Item/Item';
 import styles from './ItemList.module.css';
 import Checkbox from '@material-ui/core/Checkbox';
 
-const ItemList = ( {tasks} ) => {
+const ItemList = ( {tasks, onClickDone} ) => {
   return(<ul>
   {tasks.map( task => 
     <li className={styles.li} key={task.value}>
       <Checkbox 
-      value="checkedLeft"
-      inputProps={{
-        'aria-label': 'primary checkbox',
-      }} 
-      onChange />
+        checked={task.isDone} 
+        onClick={() => onClickDone(task.isDone)} />
       <Item value={task.value} isDone={task.isDone} />
       <Checkbox
         value="checkedRight"
