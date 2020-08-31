613d28b9f5d3f774b3b8d411979a4b3a431ed997
Sergei Golubtsov <golubtsovsergei581study@gmail.com>
2020-08-31 12:45:47 +0300

    reactRouter

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,100 +1,32 @@
 import React, { useState, useEffect } from 'react';
-import InputItem from '../InputItem/InputItem';
-import ItemList from '../ItemList/ItemList';
-import Footer from '../Footer/Footer';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import Card from '@material-ui/core/Card';
+import MenuItem from '@material-ui/core/MenuItem';
+import MenuList from '@material-ui/core/MenuList';
+
+import Todo from '../Todo/Todo';
+import About from '../About/About';
+import Contacts from '../Contacts/Contacts';
+
 import styles from './App.module.css';
 
 
+const App = () => (<Router>
+  <div className={styles.wrap}>
+    <Card className={styles.sidebar}>
+      <MenuList>
+        <Link className={styles.link} to='/'><MenuItem>Обо мне</MenuItem></Link>
+        <Link className={styles.link} to='/todo'><MenuItem>Дела</MenuItem></Link>
+        <Link className={styles.link} to='/contacts'><MenuItem>Контакты</MenuItem></Link>
+      </MenuList>
+    </Card>
 
-const App = () => {
-  const initialState = {
-    tasks: [
-      {
-        value: 'Закончить модуль по React',
-        isDone: false,
-        id: 1
-      },
-      {
-        value: 'Пройти модуль Трудоустройство',
-        isDone: false,
-        id: 2
-      },
-      {
-        value: 'Подготовится к собеседованию',
-        isDone: true,
-        id: 3
-      }
-    ],
-    count: 3,
-    error: false,
-    helperText: ""
-  };
-
-  const [tasks, setTasks] = useState(initialState.tasks);
-  const [count, setCount] = useState(initialState.count);
-  const [error, setError] = useState(initialState.error);
-  const [helperText, setHelperText] = useState(initialState.helperText);
-
-  // eslint-disable-next-line no-console
-  useEffect(() => {console.log("componentDidMount");}, []);
-  // eslint-disable-next-line no-console
-  useEffect(() => {console.log("componentDidUpdate");});
-
-  const onClickDone = (id) => {
-    const newTaskList = tasks.map((task) => {
-      const newTask = { ...task };
-
-      if (task.id === id) {
-        newTask.isDone = !task.isDone;
-      }
-
-      return newTask;
-    });
-    setTasks(newTaskList);
-  };
-
-  const onClickDelete = (id) => {
-    const newTaskList = tasks.filter((task) => {
-      return task.id !== id;
-    });
-    setTasks(newTaskList);
-    setCount((count) => count - 1);
-  };
-
-  const onClickAdd = (value) => {
-    if (value !== "") {
-      const newTasks = [
-        ...tasks,
-        {
-          value,
-          isDone: false,
-          id: count + 1
-        }
-      ];
-      setTasks(newTasks);
-      setCount((count) => count + 1);
-      setError((error) => error = false);
-      setHelperText((helperText) => helperText = "");
-    } else {
-      setError((error) => error = true);
-      setHelperText((helperText) => helperText = "Пустое поле");
-    }
-  };
-
-  return (<div className={styles.wrap}>
-    <h1 className={styles.title}>Важные дела:</h1>
-    <InputItem
-      onClickAdd={onClickAdd}
-      error={error}
-      helperText={helperText}
-    />
-    <ItemList
-      tasks={tasks}
-      onClickDone={onClickDone}
-      onClickDelete={onClickDelete}
-    />
-    <Footer count={count} />
-  </div>)
-};
+    <Card className={styles.content}>
+      <Route path='/' exact component={About} />
+      <Route path='/todo' component={Todo} />
+      <Route path='/contacts' component={Contacts} />
+    </Card>
+  </div>
+</Router>);
 
 export default App;
